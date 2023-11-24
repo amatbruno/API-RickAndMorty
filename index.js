61d77060ed6b3456bddb3d8c945874383155842d
Bruno <brunoamagar@campus.monlau.com>
2023-11-24 16:13:03 +0100

    updates-jsonplaceholder

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 
-//RICK & MORTY SECTION
+//GET RICK & MORTY API FUNCT
 function getCharacters(done) {
-    const results = fetch("https://rickandmortyapi.com/api/character/");
+	const results = fetch("https://rickandmortyapi.com/api/character/");
 
-    results
-        .then(response => response.json())
-        .then(data => {
-            done(data.results);
-        });
+	results
+		.then(response => response.json())
+		.then(data => {
+			done(data.results);
+		});
 }
 
-function displayCharacters(characters) {
-    const main = document.querySelector('main');
-    main.innerHTML = ""; // Clear the existing content
+//JSONPLACEHOLDER API FUNCT
+async function getColors() {
+	const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+	const data = await response.json();
 
-    characters.forEach(personaje => {
-        const article = document.createRange().createContextualFragment(/*html*/`
-            <article>
+	// Array of colors for variety
+	const colors = ['lightblue', 'lightgreen', 'lightcoral', 'lightgoldenrodyellow', 'lightpink', 'lightcyan', 'lightgray', 'lightsalmon', 'lightseagreen'];
+
+	// Use modulus to cycle through the colors
+	return data.map((post, index) => colors[index % colors.length]);
+}
+
+function displayCharacters(characters, colors) {
+	const main = document.querySelector('main');
+	main.innerHTML = ""; // Clear the existing content
+
+	characters.forEach((personaje, index) => {
+		const color = colors[index]; // Get the color for the current character
+
+		const article = document.createRange().createContextualFragment(/*html*/`
+			<article style="background-color: ${color}">
                 <div class="image-container">
                     <img src="${personaje.image}" alt="Personaje">
                 </div>
@@ -27,89 +41,28 @@ function displayCharacters(characters) {
                 <p>Gender: ${personaje.gender}</p>
                 <p>Specie: ${personaje.species}</p>
                 <p>Type: ${personaje.type}</p>
+				<button type="submit">Character Voice</button>
+				
             </article>
         `);
 
-        main.append(article);
-    });
+		main.append(article);
+	});
 }
 
 // Add an input event listener to capture real-time input changes
 document.getElementById('character-name').addEventListener('input', function () {
-    const characterName = this.value.trim().toLowerCase();
+	const characterName = this.value.trim().toLowerCase();
 
-    getCharacters(data => {
-        const filteredCharacters = data.filter(personaje => personaje.name.toLowerCase().includes(characterName));
-        displayCharacters(filteredCharacters);
-    });
+	getCharacters(async data => {
+		const colors = await getColors();
+		const filteredCharacters = data.filter(personaje => personaje.name.toLowerCase().includes(characterName));
+		displayCharacters(filteredCharacters, colors);
+	});
 });
 
 // Initial display of all characters
-getCharacters(data => {
-    displayCharacters(data);
+getCharacters(async data => {
+	const colors = await getColors();
+	displayCharacters(data, colors);
 });
-
-
-
-//COLORS SECTION
-let galNav   = document.querySelector('#galNav ul');
-let gallery  = document.querySelector('#gallery');
-let display  = document.querySelector('#display');
-const API    = {
-	albums : "https://jsonplaceholder.typicode.com/albums",
-	photos : "https://jsonplaceholder.typicode.com/photos"
-};
-
-// CSS classes
-let thumbCSS = [];
-let albumCSS = [];
-
-// templates
-const albumTemplate = (album) =>{
-	return `
-		<li  class 		= "album ${ albumCSS.join(" ") }" 
-				 onclick 	= "getGallery(${album.id})">
-				 ${album.title}
-		</li>`
-};
-const photoTemplate = (photo) =>{
-	return `
-	<div   class    = "thumbWrapper" 
-				 onclick  = "getPhoto( ${photo.id} )"/>
-		<img class    = "thumb ${ thumbCSS.join(" ") }" 
-			   src      = "${photo.thumbnailUrl}" 
-			   alt      = "${photo.title}" />
-			   
-		<h6>${photo.title}</h6>
-	</div>`
-};
-
-// fetch functions
-function getAlbums(){
-	fetch(API.albums)
-		.then((res) => res.json())
-		.then((data) => {
-			galNav.innerHTML = data.map(albumTemplate).join('');
-			getGallery(data[0].id)
-		});
-}
-function getGallery(id){
-	fetch(`${API.photos}?albumId=${id}`)  //query Album;
-		.then((res) => res.json())
-		.then((data) => {
-		getPhoto(data[0].id)
-		gallery.innerHTML = data.map(photoTemplate).join("");
-		
-	})
-}
-function getPhoto(id){ 
-	fetch(`${API.photos}?id=${id}`)
-		.then((res) => res.json())
-		.then((data) => {
-			display.src = data[0].url;
-			display.alt = data[0].title;
-		})
-}
-
-// fetch album
-getAlbums();
